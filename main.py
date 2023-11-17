be40dc3b34601ece4634a6ff826fc2d092b72b27
Henrique Furtado <63440926+HenriqueFurtado-Dev@users.noreply.github.com>
2023-11-17 13:13:37 +0000

    Criado o menu

--- main.py
+++ main.py
@@ -5,16 +5,30 @@ def menu():
     print("4 - Ver informacoes de um usuario")
     print("5 - Ver procedimentos tecnicos")
     print("6 - Alterar estado")
-    escolha_menu = input("Escolha uma opcao: ")
+    escolha_menu = int(input("Escolha uma opcao: "))
 
     return escolha_menu
 
 def verificar_menu():
-    print(escolha_menu)
+    escolha_menu = menu()
+    if escolha_menu == 1:
+        print("1")
+    elif escolha_menu == 2:
+        print("2")
+    elif escolha_menu == 3:
+        print("3")
+    elif escolha_menu == 4:
+        print("4")
+    elif escolha_menu == 5:
+        print("5")
+    elif escolha_menu == 6:
+        print("6")
+    else: 
+        print("Valor inválido")
+
 
 def main():
     print("Seja bem vindo ao Gerenciador da SITREM")
-    menu()
     verificar_menu()
 
 if __name__ == '__main__':
